56fb549df2abd280174e4f7dbc4ac6614a99a8ee
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-27 02:00:43 -0500

    :tada: First commit

---
+++ backend/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.listen(3000, () => {
+  console.log('UP');
+});
