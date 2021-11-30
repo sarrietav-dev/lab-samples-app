df38981494b7f7c91b7af3fa7555781cae78de0d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-29 19:28:32 -0500

    💄 Add buttons and heading

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,13 +5,20 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div style={{ height: '100vh' }}>
       <nav className="navbar navbar-dark bg-primary">
         <div className="container-fluid">
           <p className="navbar-brand">Lab</p>
         </div>
       </nav>
-    </>
+      <div className="d-flex h-100 flex-column align-items-center justify-content-evenly">
+        <h1>Bienvenido, David Null</h1>
+        <div className="d-flex flex-column">
+          <button className="btn btn-primary my-2">Agendar Cita</button>
+          <button className="btn btn-info my-2">Ver Resultados</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
