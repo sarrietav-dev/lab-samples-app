6ed3d42a20b1b6fdebf29443fc36838daf4b342e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-29 20:33:57 -0500

    💄 Add table to the user page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,6 +10,24 @@ const UserHome: NextPage = () => {
       </nav>
       <div className="d-flex h-100 flex-column align-items-center justify-content-evenly">
         <h1>Bienvenido, David Null</h1>
+        <table className="table table-hover mx-4">
+          <thead>
+            <tr>
+              <th scope="col">Fecha</th>
+              <th scope="col">Tipo de examen</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">9/9/9</th>
+              <td>Sangre</td>
+            </tr>
+            <tr>
+              <th scope="row">10/10/10</th>
+              <td>Sangre</td>
+            </tr>
+          </tbody>
+        </table>
         <div className="d-flex flex-column">
           <button className="btn btn-primary my-2">Agendar Cita</button>
           <button className="btn btn-info my-2">Ver Resultados</button>
