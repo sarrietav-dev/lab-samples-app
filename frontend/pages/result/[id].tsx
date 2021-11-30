20c9d07d64ffbb7c038acc7107e06f50efb0045f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-30 14:53:33 -0500

    💄 Use grid to arrange items
    Also gave them padding

--- frontend/pages/result/[id].tsx
+++ frontend/pages/result/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
 
 const ResultPage = () => {
   const router = useRouter();
@@ -13,20 +13,48 @@ const ResultPage = () => {
           <Navbar.Brand>Laboratorio</Navbar.Brand>
         </Container>
       </Navbar>
-      <h1>{id}</h1>
-      <i className="bi bi-person-circle" style={{ fontSize: '2rem' }}></i>
-      <span>David Null</span>
-      <i className="bi bi-clock" style={{ fontSize: '2rem' }}></i>
-      <span>Noviembre 16, 2021. 9:00 A.M</span>
-      <i className="bi bi-file-medical" style={{ fontSize: '2rem' }}></i>
-      <span>Sangre</span>
-      <h2>Detalles</h2>
-      <textarea
-        value={'bslasalbsalsbalbsl'}
-        cols={30}
-        rows={10}
-        disabled
-      ></textarea>
+      <main className="p-4">
+        <h1>{id}</h1>
+        <Container className="d-flex flex-column my-5">
+          <Row className="align-items-center">
+            <Col className="col-auto">
+              <i
+                className="bi bi-person-circle"
+                style={{ fontSize: '2rem' }}
+              ></i>
+            </Col>
+            <Col>
+              <span>David Null</span>
+            </Col>
+          </Row>
+          <Row className="align-items-center">
+            <Col className="col-auto">
+              <i className="bi bi-clock" style={{ fontSize: '2rem' }}></i>
+            </Col>
+            <Col>
+              <span>Noviembre 16, 2021. 9:00 A.M</span>
+            </Col>
+          </Row>
+          <Row className="align-items-center">
+            <Col className="col-auto">
+              <i
+                className="bi bi-file-medical"
+                style={{ fontSize: '2rem' }}
+              ></i>
+            </Col>
+            <Col>
+              <span>Sangre</span>
+            </Col>
+          </Row>
+        </Container>
+        <h2 className="mb-4">Detalles</h2>
+        <textarea
+          value={'bslasalbsalsbalbsl'}
+          cols={30}
+          rows={10}
+          disabled
+        ></textarea>
+      </main>
     </div>
   );
 };
