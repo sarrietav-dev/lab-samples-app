8c391757484e3c25a57d54e60c416118a03eb586
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-30 17:58:00 -0500

    ♻️ Change individual margins to gap

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -4,14 +4,14 @@ const LoginPage = () => {
   return (
     <Form
       method="POST"
-      className="vh-100 d-flex flex-column justify-content-center"
+      className="vh-100 d-flex flex-column justify-content-center gap-4"
       style={{ padding: '4rem' }}
     >
-      <Form.Group controlId="email" className="mt-3 mb-3">
+      <Form.Group controlId="email">
         <Form.Label>Correo Electronico</Form.Label>
         <Form.Control type="email" placeholder="nombre@ejemplo.com" />
       </Form.Group>
-      <Form.Group controlId="passwords" className="mt-2 mb-3">
+      <Form.Group controlId="passwords">
         <Form.Label>Contraseña</Form.Label>
         <Form.Control type="password" placeholder="Contraseña" />
       </Form.Group>
