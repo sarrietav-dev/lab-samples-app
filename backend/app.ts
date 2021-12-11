2bac303c32f8e7faebeda72df3a75c55493def5d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-11 18:36:24 -0500

    🔧 Use CORS middleware

--- backend/app.ts
+++ backend/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(helmet());
 
