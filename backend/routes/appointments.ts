d9cf343b4cd3bc766a37fd40c7148877ddd9fb8a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 13:24:53 -0500

    ✨ Create an appointment  POST endpoint

--- backend/routes/appointments.ts
+++ backend/routes/appointments.ts
@@ -1,7 +1,24 @@
 import { Router } from 'express';
+import Appointment from '../models/Appointment';
+import { postValidation } from './validation/appointments.validation';
 
 const router = Router();
 
-router.post('/', (req, res) => {});
+router.post('/', async (req, res) => {
+  const { error, value } = (await postValidation()).validate(req.body);
+
+  if (error) return res.status(400).json({ error: error.message });
+
+  const { date, type } = value;
+
+  const appointment = new Appointment({
+    date,
+    type,
+  });
+
+  await appointment.save();
+
+  return res.status(200);
+});
 
 export default router;
