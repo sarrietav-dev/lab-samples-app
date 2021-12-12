1d0e5aa260ee660dc82c79712efd807252605894
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 12:35:32 -0500

    ♻️ Give types to TestType model

--- backend/models/TestType.ts
+++ backend/models/TestType.ts
@@ -1,7 +1,13 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const TestTypeSchema = new Schema({
-  name: String,
+interface TestType {
+  name: string;
+  activeTests: number;
+}
+
+const TestTypeSchema = new Schema<TestType>({
+  name: Schema.Types.String,
+  activeTests: Schema.Types.Number,
 });
 
-export default model("TestType", TestTypeSchema);
+export default model<TestType>('TestType', TestTypeSchema);
