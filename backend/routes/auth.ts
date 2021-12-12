8948e26a7effded78facead012b148887b0c4ca0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 00:22:14 -0500

    ✨ (routes): create sign up route
    Closes #35

---
+++ backend/routes/auth.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+
+const router = Router();
+
+router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const newUser = new User({
+    name: username,
+    email,
+    password: bcrypt.hashSync(password, 8),
+    role: 'client',
+  });
+
+  await newUser.save();
+
+  res.status(200).json({
+    success: true
+  })
+});
