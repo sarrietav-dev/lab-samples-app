40af11ec5edb6594ad75ffe0055c9e5f7a13cc25
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 17:46:13 -0500

    ✨ Use router in the app

--- backend/app.ts
+++ backend/app.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import authRoute from './routes/auth';
 import appointmentsRoute from './routes/appointments';
+import testTypesRoute from './routes/test-types';
 import { authenticateJWT } from './middleware/auth-middleware';
 
 dotenv.config();
@@ -22,6 +23,7 @@ mongoose.connect(process.env.MONGODB_CONN_STRING!, (_) =>
 
 app.use('/api/auth', authRoute);
 app.use('/api/appointments', authenticateJWT, appointmentsRoute);
+app.use('/api/test-types', testTypesRoute);
 
 app.get('/', (_, res) => {
   res.send('Hi');
