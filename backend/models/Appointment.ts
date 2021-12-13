7afa10bce5d4c9184f9fa9c5823539fea07135ff
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 22:51:34 -0500

    ✨ Add resolved and details properties to the appointment schema

--- backend/models/Appointment.ts
+++ backend/models/Appointment.ts
@@ -4,12 +4,16 @@ interface AppointmentSchema {
   userId: string;
   date: Date;
   type: String;
+  resolved: boolean;
+  details: String;
 }
 
 const AppointmentSchema = new Schema<AppointmentSchema>({
   userId: Schema.Types.ObjectId,
   date: Schema.Types.Date,
   type: Schema.Types.String,
+  resolved: Schema.Types.Boolean,
+  details: Schema.Types.String,
 });
 
 export default model<AppointmentSchema>('Appointment', AppointmentSchema);
