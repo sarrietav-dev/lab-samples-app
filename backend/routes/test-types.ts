4e5375217c822e4d3a3ac2534f1b712b0e0edd1b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-12 23:51:25 -0500

    ✨ Create endpoint to edit a type of test Fixes #37

--- backend/routes/test-types.ts
+++ backend/routes/test-types.ts
@@ -30,4 +30,32 @@ router.post('/', async (req, res) => {
   res.status(201).send();
 });
 
+router.patch('/:id', async (req, res) => {
+  const { error } = testTypeValidation.validate(req.body);
+
+  if (error) return res.status(400).json({ message: error.message });
+
+  const oldTestType = await TestType.findById(req.params.id);
+
+  if (!oldTestType)
+    return res.status(404).json({ message: "The test type doesn't exist" });
+
+  const { name } = req.body;
+
+  const testTypesWithTheSameName = await TestType.find({
+    name: { $regex: name, $options: 'i' },
+  });
+
+  if (testTypesWithTheSameName.length !== 0)
+    return res.status(409).json({
+      message: "There's a test whose name is equal to the given test",
+    });
+
+  oldTestType.name = name;
+
+  await oldTestType.save();
+
+  res.status(200).send();
+});
+
 export default router;
